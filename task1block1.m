0fd1e62bb083b7450a70dbddb7235ef80d9e991b
Onofre Martorell <>
2016-10-05 12:19:42 +0200

    Esquema de el trabajo a hacer en task 1

--- task1block1.m
+++ task1block1.m
@@ -7,11 +7,6 @@ addpath('./evaluation')
 addpath('./colorspace')
 addpath('./circular_hough')
 
-%Save the current working directory and change it to the folder where
-%the gt files are
-% OldFolder = pwd;
-% NewFolder = './train/gt';
-% cd(NewFolder)
 
 %Read all the files from the directory
 Folder_gt = './train/gt';
@@ -29,64 +24,95 @@ for i = 1:length(archivos)
             name = archivos(i).name;
             file = strcat(NewFolder, '/', name);
             [annotations, ~] = LoadAnnotations(file);            
-            Total_signs = Total_signs + size(annotations,1);
+            Total_signs = Total_signs + size(annotations, 1);
         end
     end
 end
 
+%The characteristics will be saved in a matrix where each row corresponds
+%to a sign and the columns are:
+%1: File where the sign appears
+%2: Area of the bound box containing the sign
+%3: form ratio = width/height
+%4: filling ratio = mask_area / bbox_area
+%5: type of sign
 
-%Creamos una matriz donde se guardaran los filename en orden en función
-%a su tamaño (de menor área de bounding box a mayor en cada columna) y
-%en 6 filas en función al tipo de señal (A,B,C,D,E,F), así como
-%vectores auxiliares para cada tipo.
-
-auxA = {};
-auxB = {};
-auxC = {};
-auxD = {};
-auxE = {};
-auxF = {};
-
-
-%For all the files read
+Sign_characteristics = zeros(Total_signs, 5);
+%For each image
 for i = 1:length(archivos)
     
     %Ensure the file is a .txt
     if archivos(i).isdir == 0,
-        if strcmp(archivos(i).name(end-2:end),'txt') == 1
+        if strcmp(archivos(i).name(end-2:end),'txt') == 1  
             
             name = archivos(i).name;
-            name = strcat(NewFolder, '/', name);
-            %Open file and read line
-            fileID = fopen(name, 'r');
-            tline = fgetl(fileID);
-            
-            if (strfind(tline,'A') ~= 0)
-                auxA{end + 1} = name;
+            file = strcat(NewFolder, '/', name);
+            [annotations, Signs] = LoadAnnotations(file);
+            %For each sign
+            for j = 1:size(annotations, 1)
+                %Compute the characteristics
                 
-            elseif (strfind(tline,'B') ~= 0)
-                auxB{end + 1} = name;
+                %Char 2
                 
-            elseif (strfind(tline,'C') ~= 0)
-                auxC{end + 1} = name;
+                %Char 3
                 
-            elseif (strfind(tline,'D') ~= 0)
-                auxD{end + 1} = name;
+                %Char 4
                 
-            elseif (strfind(tline,'E') ~= 0)
-                auxE{end + 1} = name;
-                
-            elseif (strfind(tline,'F') ~= 0)
-                auxF{end + 1} = name;
-                
-            elseif tline == -1
-                fclose(fileID);
-                continue;
-            end
-            fclose(fileID);
+                %Char 5
+            end    
         end
     end
 end
+% %Creamos una matriz donde se guardaran los filename en orden en función
+% %a su tamaño (de menor área de bounding box a mayor en cada columna) y
+% %en 6 filas en función al tipo de señal (A,B,C,D,E,F), así como
+% %vectores auxiliares para cada tipo.
+% 
+% auxA = {};
+% auxB = {};
+% auxC = {};
+% auxD = {};
+% auxE = {};
+% auxF = {};
+% 
+% 
+% %For all the files read
+% for i = 1:length(archivos)
+%     
+%     %Ensure the file is a .txt
+%     if archivos(i).isdir == 0,
+%         if strcmp(archivos(i).name(end-2:end),'txt') == 1
+%             
+%             name = archivos(i).name;
+%             name = strcat(NewFolder, '/', name);
+%             %Open file and read line
+%             fileID = fopen(name, 'r');
+%             tline = fgetl(fileID);
+%             
+%             if (strfind(tline,'A') ~= 0)
+%                 auxA{end + 1} = name;
+%                 
+%             elseif (strfind(tline,'B') ~= 0)
+%                 auxB{end + 1} = name;
+%                 
+%             elseif (strfind(tline,'C') ~= 0)
+%                 auxC{end + 1} = name;
+%                 
+%             elseif (strfind(tline,'D') ~= 0)
+%                 auxD{end + 1} = name;
+%                 
+%             elseif (strfind(tline,'E') ~= 0)
+%                 auxE{end + 1} = name;
+%                 
+%             elseif (strfind(tline,'F') ~= 0)
+%                 auxF{end + 1} = name;
+%                 
+%             elseif tline == -1
+%                 fclose(fileID);
+%                 continue;
+%             end
+%             fclose(fileID);
+%         end
+%     end
+% end
 
-%Move back to old working directory
-cd(OldFolder)
