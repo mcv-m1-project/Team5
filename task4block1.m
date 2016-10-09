c0e260612654431fd5d82201ef34114222da3fcd
Onofre Martorell <>
2016-10-09 20:13:46 +0200

    mas de la task 4

--- task4block1.m
+++ task4block1.m
@@ -14,17 +14,26 @@ TP_images = zeros(files, 1);
 FP_images = zeros(files, 1);
 FN_images = zeros(files, 1);
 TN_images = zeros(files, 1);
+Precision_images = zeros(files, 1);
+Accuracy_images = zeros(files, 1);
+Specificit_images = zeros(files, 1);
+Sensitivity_images = zeros(files, 1);
 
 for i = 1:files
     mask = imread(strcat('./train/mask', Files(i)));
     pixelAnnotation = mask > 128;
     %Compute pixelCandidates!!!!!
     [pixelTP, pixelFP, pixelFN, pixelTN] = PerformanceAccumulationPixel(pixelCandidates, pixelAnnotation);
+    [pixelPrecision, pixelAccuracy, pixelSpecificity, pixelSensitivity] = PerformanceEvaluationPixel(pixelTP, pixelFP, pixelFN, pixelTN);
     TP_images(i) = pixelTP;
     FP_images(i) = pixelFP;
     FN_images(i) = pixelFN;
     TN_images(i) = pixelTN;
-end  
+    Precision_images(i) = pixelPrecision;
+    Accuracy_images(i) = pixelAccuracy;
+    Specificit_images(i) = pixelSpecificity;
+    Sensitivity_images(i) = pixelSensitivity;
+end
 
 average_TP = mean(TP_images);
 average_FP = mean(FP_images);
