c20980608120d272ff00ff0ce9816a6d9f3fc132
Roque <roque.rodriguez.outeiral@gmail.com>
2016-10-15 23:58:09 +0200

    Subo el Mask4 (task3block1) con las modificaciones de hoy

--- week1/Mask4.m
+++ week1/Mask4.m
@@ -3,20 +3,21 @@
 % colorSpace=2;
 % Mask(path_images,colorSpace)
 % function Mask2(path_images,path_GT,colorSpace)
-function Mask4(path_images, colorSpace)
+function theTime = Mask4(path_images, colorSpace)
 
 
 mkdir([path_images 'Masks'])
 Imgs = dir([path_images '/*.jpg']);
 %Imgs_GT = dir([path_GT '/*.png']);
 ext='.jpg';
-
+theTime = zeros(362,1);
 % colorSpace=2;
 
 switch colorSpace
     case 1
         mkdir([path_images 'Masks/OtsuRGB'])
         for numImagen = 1:length(Imgs)
+            tic
             close all;	% Close all figure windows
             rgbImage = imread(strcat(path_images,Imgs(numImagen).name));
             %GT = imread(strcat(path_GT,Imgs_GT(numImagen).name));
@@ -40,12 +41,14 @@ switch colorSpace
             %figure, imshow(Mask)
             imwrite(Mask,[path_images 'Masks/OtsuRGB/' Imgswithoutext...
                 '_mask.jpg' ]);
+            theTime(numImagen) = toc;
             
         end
         
     case 2
         mkdir([path_images 'Masks/HSV'])
         for numImagen=1:length(Imgs)
+       tic
             rgbImage = imread(strcat(path_images,Imgs(numImagen).name));
             
             Imgswithoutext=strrep(Imgs(numImagen).name,ext,'');
@@ -60,31 +63,32 @@ switch colorSpace
             vImage = hsvImage(:,:,3);
             
             % A "threshold" of the hsv components of pixels must be an interval
-            hredInterval = [320 60]; % Range of hue values considered 'red'
-            hblueInterval = [180 300]; % Range of hue values considered 'blue'
+            hredInterval = [350 20]; % Range of hue values considered 'red'
+            hblueInterval = [200 230]; % Range of hue values considered 'blue'
             
             sInterval = [0.5 1]; % Minimum saturation value to exclude noise
             vInterval = [0.1 1];
             
-            redMask = (hImage >= hredInterval(1) | hImage <= hredInterval(2)) & ...
-                sImage >= sInterval(1) & sImage <= sInterval(2) &...
+            redMask = (hImage >=hredInterval(1) | hImage <= hredInterval(2)) &... 
+                  sImage >= sInterval(1) & sImage <= sInterval(2) &...
                 vImage >= vInterval(1) & vImage <= vInterval(2);
-            %figure,imshow(redMask)
+
             blueMask = (hImage >= hblueInterval(1) & hImage <= hblueInterval(2)) &...
-                sImage >= sInterval(1) & sImage <= sInterval(2) &...
+                  sImage >= sInterval(1) & sImage <= sInterval(2) &...
                 vImage >= vInterval(1) & vImage <= vInterval(2);
-            %figure,imshow(blueMask)
+     
             
             Mask = redMask | blueMask;
             %figure,imshow(Mask)
             imwrite(Mask,[path_images 'Masks/HSV/' Imgswithoutext...
                 '_mask.jpg' ]);
-            
-            
+            theTime(numImagen) = toc;
+  
         end
     case 3
         mkdir([path_images 'Masks/Lab'])
         for numImagen=1:length(Imgs)
+            tic
             rgbImage = imread(strcat(path_images,Imgs(numImagen).name));
             Imgswithoutext=strrep(Imgs(numImagen).name,ext,'');
             labImage = rgb2lab(rgbImage);
@@ -96,10 +100,12 @@ switch colorSpace
             Mask = redMask | blueMask;
             imwrite(Mask,[path_images 'Masks/Lab/' Imgswithoutext...
                 '_mask.jpg' ]);
+            theTime(numImagen) = toc;
         end
     case 4
         mkdir([path_images 'Masks/HSV&RGB'])
         for numImagen=1:length(Imgs)
+            tic
             rgbImage = imread(strcat(path_images,Imgs(numImagen).name));
             
             Imgswithoutext=strrep(Imgs(numImagen).name,ext,'');
@@ -114,8 +120,8 @@ switch colorSpace
             vImage = hsvImage(:,:,3);
             
             % A "threshold" of the hsv components of pixels must be an interval
-            hredInterval = [320 60]; % Range of hue values considered 'red'
-            hblueInterval = [180 300]; % Range of hue values considered 'blue'
+            hredInterval = [350 20]; % Range of hue values considered 'red'
+            hblueInterval = [200 230]; % Range of hue values considered 'blue'
             
             sInterval = [0.5 1]; % Minimum saturation value to exclude noise
             vInterval = [0.1 1];
@@ -153,5 +159,30 @@ switch colorSpace
             %figure,imshow(Mask)
             imwrite(Mask,[path_images 'Masks/HSV&RGB/' Imgswithoutext...
                 '_mask.jpg' ]);
+            theTime(numImagen) = toc;
         end
+    case 5
+        mkdir([path_images 'Masks/RGBManual'])
+        for numImagen=1:length(Imgs)
+        tic
+        rgbImage = imread(strcat(path_images,Imgs(numImagen).name));
+            %GT = imread(strcat(path_GT,Imgs_GT(numImagen).name));
+            
+        Imgswithoutext = strrep(Imgs(numImagen).name,ext,'');
+            
+                redMask = rgbImage(:,:,1) > 50 &...
+                rgbImage(:,:,2) < 40 &...
+                rgbImage(:,:,3) < 40;
+            
+                blueMask = rgbImage(:,:,1) < 50 &...
+                rgbImage(:,:,2) < 60 &...
+                rgbImage(:,:,3) > 60;
+            
+            Mask = redMask | blueMask;
+            imwrite(Mask,[path_images 'Masks/RGBManual/' Imgswithoutext...
+                '_mask.jpg' ]);
+   
+         theTime(numImagen) = toc;
+        end
+
 end
