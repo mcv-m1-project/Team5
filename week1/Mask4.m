89e31d649c4e877ecbae5cbc7bb492836b002c23
Lidia Talavera <lidia_tama@hotmail.com>
2016-10-16 17:56:46 +0200

    código comentado

--- week1/Mask4.m
+++ week1/Mask4.m
@@ -1,3 +1,23 @@
+% Task 3. Task3block1 is a function that perfoms a segmentation of the red
+% and blue colors over an input RGB image. The function has the next 
+% three input parameters (IP) and one output parameter (OP).
+% 
+% path_images:(IP) Is the path where we can find all the dataset.
+% Imgs:(IP) List of images that have been chosen as training set (Task 2)
+% on which segmentation will be performed.
+% colorSpace:(IP) number that represents the method applied to implement
+% the segmentation.
+%                                  ------Method------
+%       colorSpace 1 -> RGB with manual selection of the thresholds.
+%       colorSpace 2 -> RGB using Otsu?s segmentation method.
+%       colorSpace 3 -> Segmentation on HSV color space.
+%       colorSpace 4 -> Segmentation on Lab color space.
+%       colorSpace 5 -> Segmentation on YUV color space.
+%       colorSpace 6 -> Combination of HSV and RGB color spaces.
+%
+% time:(OP) number that represents the average of time that is needed for
+% execute the segmentation for a specific method.
+
 function time = Mask4(path_images, Imgs, colorSpace)
 mkdir([path_images 'Masks'])
 Imgs = dir([path_images '/*.jpg']);
@@ -15,12 +35,14 @@ switch colorSpace
             rgbImage = imread(strcat(path_images,Imgs(numImagen).name));
             Imgswithoutext = strrep(Imgs(numImagen).name,ext,'');
             
+            % Red segmentation
             redMask = rgbImage(:,:,1) > 50 & rgbImage(:,:,2) < 40 &...
                 rgbImage(:,:,3) < 40;
-            
+            % Blue segmentation
             blueMask = rgbImage(:,:,1) < 50 & rgbImage(:,:,2) < 60 &...
                 rgbImage(:,:,3) > 60;
             
+            % Combination of the red and blue segmentation -> binary image
             Mask = redMask | blueMask;
             
             imwrite(Mask,[path_images 'Masks/RGBManual/' Imgswithoutext...
@@ -41,14 +63,15 @@ switch colorSpace
             greenThresh = multithresh(rgbImage(:,:,2),2);
             blueThresh = multithresh(rgbImage(:,:,3),2);
             
+            % Red segmentation
             redMask = rgbImage(:,:,1) > redThresh(1) &...
                 rgbImage(:,:,2) < greenThresh(1) &...
                 rgbImage(:,:,3) < blueThresh(1);
-            
+            % Blue segmentation
             blueMask = rgbImage(:,:,1) < redThresh(1) &...
                 rgbImage(:,:,2) < greenThresh(1) &...
                 rgbImage(:,:,3) > blueThresh(1);
-
+            % Combination of the red and blue segmentation -> binary image
             Mask = redMask | blueMask;
             
             imwrite(Mask,[path_images 'Masks/OtsuRGB/' Imgswithoutext...
@@ -71,21 +94,22 @@ switch colorSpace
             sImage = hsvImage(:,:,2);
             vImage = hsvImage(:,:,3);
             
-            % A "threshold" of the hsv components of pixels must be an interval
+            % A threshold of the hsv components of pixels must be an interval
             hredInterval = [350 20]; % Range of hue values considered red
             hblueInterval = [200 230]; % Range of hue values considered blue            
             sInterval = [0.5 1]; % Minimum saturation value to exclude noise
             vInterval = [0.1 1];
             
+            % Red segmentation
             redMask = (hImage >=hredInterval(1) | hImage <= hredInterval(2))...
                 & sImage >= sInterval(1) & sImage <= sInterval(2) &...
                 vImage >= vInterval(1) & vImage <= vInterval(2);
-
+            % Blue segmentation
             blueMask = (hImage >= hblueInterval(1) & hImage <= hblueInterval(2))...
                 & sImage >= sInterval(1) & sImage <= sInterval(2) &...
                 vImage >= vInterval(1) & vImage <= vInterval(2);
      
-            
+            % Combination of the red and blue segmentation -> binary image
             Mask = redMask | blueMask;
             
             imwrite(Mask,[path_images 'Masks/HSV/' Imgswithoutext...
@@ -106,11 +130,17 @@ switch colorSpace
             labImage = rgb2lab(rgbImage);
             
             labImage = zeros(size(rgbImage));
-            [labImage(:, :, 1), labImage(:, :, 2), labImage(:, :, 3)] = RGB2Lab_own(rgbImage(:, :, 1), rgbImage(:, :, 2), rgbImage(:, :, 3));
-%             labImage = rgb2lab(rgbImage);
+            [labImage(:, :, 1), labImage(:, :, 2), labImage(:, :, 3)] =...
+                RGB2Lab_own(rgbImage(:, :, 1), rgbImage(:, :, 2),...
+                rgbImage(:, :, 3));
+            %labImage = rgb2lab(rgbImage);
+            
+            % Red segmentation
             redMask = labImage(:,:,2)==1 & labImage(:,:,3)==1;
+            % Blue segmentation
             blueMask = labImage(:,:,2)==0 & labImage(:,:,3)==0;
             
+            % Combination of the red and blue segmentation -> binary image
             Mask = redMask | blueMask;
             
             imwrite(Mask,[path_images 'Masks/Lab/' Imgswithoutext...
@@ -129,13 +159,15 @@ switch colorSpace
             
             % Convert RGB image to YUV space
             yuvImage = rgb2yuv(rgbImage);
-        
+            
+            % Red segmentation
             redMask = yuvImage(:,:,2) > 110 & yuvImage(:,:,2) < 130 & ...
                 yuvImage(:,:,3) > 135 & yuvImage(:,:,3) < 165;
-            
+            % Blue segmentation
             blueMask = yuvImage(:,:,2) > 140 & yuvImage(:,:,2) < 170 & ...
                 yuvImage(:,:,3) > 100 & yuvImage(:,:,3) < 120;
             
+            % Combination of the red and blue segmentation -> binary image
             Mask = redMask | blueMask;
             
             imwrite(Mask,[path_images 'Masks/YUV/' Imgswithoutext...
@@ -158,21 +190,24 @@ switch colorSpace
             sImage = hsvImage(:,:,2);
             vImage = hsvImage(:,:,3);
             
-            % A "threshold" of the hsv components of pixels must be an interval
+            % A threshold of the hsv components of pixels must be an interval
             hredInterval = [350 20]; % Range of hue values considered 'red'
             hblueInterval = [200 230]; % Range of hue values considered 'blue'
             
             sInterval = [0.5 1]; % Minimum saturation value to exclude noise
             vInterval = [0.1 1];
             
+            % Red segmentation in HSV color space
             redMask = (hImage >= hredInterval(1) | hImage <= hredInterval(2)) & ...
                 sImage >= sInterval(1) & sImage <= sInterval(2) &...
                 vImage >= vInterval(1) & vImage <= vInterval(2);
-            
+            % Blue segmentation in HSV color space
             blueMask = (hImage >= hblueInterval(1) & hImage <= hblueInterval(2)) &...
                 sImage >= sInterval(1) & sImage <= sInterval(2) &...
                 vImage >= vInterval(1) & vImage <= vInterval(2);
             
+            % Combination of the red and blue segmentation on HSV color
+            % space -> binary image
             Maskhsv = redMask | blueMask;
 
             % RGB
@@ -180,15 +215,20 @@ switch colorSpace
             greenThresh = multithresh(rgbImage(:,:,2),2);
             blueThresh = multithresh(rgbImage(:,:,3),2);
             
+            % Red segmentation in RGB color space
             redMaskrgb = rgbImage(:,:,1) > redThresh(1) &...
                 rgbImage(:,:,2) < greenThresh(1) &...
                 rgbImage(:,:,3) < blueThresh(1);
+            % Blue segmentation in RGB color space
             blueMaskrgb = rgbImage(:,:,1) < redThresh(1) &...
                 rgbImage(:,:,2) < greenThresh(1) &...
                 rgbImage(:,:,3) > blueThresh(1);
             
-
+            % Combination of the red and blue segmentation on RGB color
+            % space -> binary image
             Maskrgb = redMaskrgb | blueMaskrgb;
+            
+            %Combination of the HSV and RGB segmentation
             Mask = Maskhsv & Maskrgb;
             
             imwrite(Mask,[path_images 'Masks/HSV&RGB/' Imgswithoutext...
