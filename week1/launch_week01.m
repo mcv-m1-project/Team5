4a34701f65df15e1cd37278b8b2662abfe3a7673
Onofre Martorell <>
2016-10-17 12:15:39 +0200

    Computation of test dataset

--- week1/launch_week01.m
+++ week1/launch_week01.m
@@ -13,7 +13,7 @@ colorSp = [         1           2      3     4     5        6   ];
 % Compute the files of the train set
 [ files_train, files_validate ] = task2block1( directory );
 
-
+%%
 %Evaluate all methods ans save its metrices for train set
 metrix_methods_train = zeros(10, 6);
 path_images_write = './train_result';
@@ -36,17 +36,27 @@ for i = 1:6
 end
 metrix_methods_validate(:, i) = metr_method;
 save('./Results/metrix_methods_validate', 'metrix_methods_validate');
-
+%%
+% Evaluate all methods for the test set
+path_images_write = './test_result';
+files_test = ListFiles(directory_test);
+for i = 1:size(files_test)
+    files_test(i).name = files_test(i).name(1:length(files_test(i).name)-4);
+end   
+for i = 1:6
+    pixel_method = colorSp(i);
+    average_time = Task3block1(directory_test, files_test, path_images_write, pixel_method);
+end
 %%
 %Evaluate all methods ans save its metrices for test set (you need masks)
-path_images_write = './train_test';
+path_images_write = './test_result';
 files_test = ListFiles(directory_test);
 for i = 1:size(files_test)
     files_test(i).name = files_test(i).name(1:length(files_test(i).name)-4);
 end    
 
 metrix_methods_test = zeros(10, 6);
-for i = 1:1
+for i = 1:6
     pixel_method = colorSp(i);
     metr_method = SignDetection(pixel_method , files_test, directory_test, path_images_write );
     metrix_methods_test(:, i) = metr_method;
