5eb5c95005c0bba3cee04e25b7e9c2e6376982cf
Onofre Martorell <>
2016-10-22 16:02:22 +0200

    task3 ejecutada correctamente

--- week2/launch_task3_w2.m
+++ week2/launch_task3_w2.m
@@ -1,6 +1,6 @@
 clear all
 close all
-
+clc
 %Directory where the masks of the different sets are placed
 directory_read = '../Results/week_01';
 directory_mask_train = '../Images/train'; 
@@ -11,8 +11,6 @@ load(strcat(directory_read, '/names_files_train'), 'files_train');
 load(strcat(directory_read, '/names_files_validate'), 'files_validate'); 
 
 %Names of the different methods we have used for the segmentation
-colorSpaces = {'RGBManual' 'HSV' 'YUV' 'HSV&RGB'};
-colorSp = [         1        2     3       4     ];
 colorSpaces = {'RGBManual' 'HSV' 'YUV' 'HSV&RGB' 'histBP'};
 colorSp = [         1        2     3       4         5];
 %We create the colorSp vector because the switch works better with numbers
@@ -23,10 +21,9 @@ directory_read_train = strcat(directory_read, '/train_result');
 
 directory_write_train = strcat(directory_write, '/train_result');
 
-metrix_methods_train = zeros(10, 4);
-for i = 1:4
 metrix_methods_train = zeros(10, length(colorSp));
 for i = 1:length(colorSp)
+    
     pixel_method = colorSp(i);
     metr_method = SignDetection_t3w2( pixel_method, files_train, directory_read_train, directory_write_train, directory_mask_train );
     metrix_methods_train(:, i) = metr_method;
@@ -38,10 +35,10 @@ save(strcat(directory_write, '/metrix_methods_train'), 'metrix_methods_train');
 %Evaluate all methods ans save its metrices for validate set
 directory_read_validate = strcat(directory_read, '/validate_result');
 directory_write_validate = strcat(directory_write, '/validate_result');
-metrix_methods_validate = zeros(10, 4);
-for i = 1:4
+
 metrix_methods_validate = zeros(10, length(colorSp));
-for i = 1:1
+for i = 1:length(colorSp)
+    
     pixel_method = colorSp(i);
     metr_method = SignDetection_t3w2( pixel_method, files_validate, directory_read_validate, directory_write_validate, directory_mask_train );
     metrix_methods_validate(:, i) = metr_method;
@@ -60,8 +57,9 @@ for i = 1:size(files_test)
     files_test(i).name = files_test(i).name(1:length(files_test(i).name)-4);
 end   
 Masks = files_test;
-for i = 1:4
+for i = 1:length(colorSp)
     pixel_method = colorSp(i);
+    
     switch pixel_method
         case 1
             path_images_write = strcat(path_images_write, '/RGBManual/');
