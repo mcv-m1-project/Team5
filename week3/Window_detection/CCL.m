0537b972ea0649c93479bcf55518054555809022
Idoia Ruiz <idoiaruizl@gmail.com>
2016-10-29 16:44:38 +0200

    bugfix for empty masks

--- week3/Window_detection/CCL.m
+++ week3/Window_detection/CCL.m
@@ -3,16 +3,18 @@ function windowCandidates = CCL(mask,train_param)
 %Component Labeling
 windowCandidates = struct('x', [], 'y', [], 'w',[], 'h', []);
 
-CC = bwconncomp(mask);
-windowProps=regionprops(CC,'all');
+if max(max(mask))>0
+    CC = bwconncomp(mask);
+    windowProps=regionprops(CC,'all');
 
-windowProps=discard_CCL_regions(windowProps,train_param);%Discard BBs
+    windowProps=discard_CCL_regions(windowProps,train_param);%Discard BBs
 
-for j=size(windowProps,1):-1:1
-    windowCandidates(j).x=round(windowProps(j).BoundingBox(1));
-    windowCandidates(j).y=round(windowProps(j).BoundingBox(2));
-    windowCandidates(j).w=windowProps(j).BoundingBox(3);
-    windowCandidates(j).h=windowProps(j).BoundingBox(4);    
+    for j=size(windowProps,1):-1:1
+        windowCandidates(j).x=round(windowProps(j).BoundingBox(1));
+        windowCandidates(j).y=round(windowProps(j).BoundingBox(2));
+        windowCandidates(j).w=windowProps(j).BoundingBox(3);
+        windowCandidates(j).h=windowProps(j).BoundingBox(4);    
+    end
 end
 
 end
