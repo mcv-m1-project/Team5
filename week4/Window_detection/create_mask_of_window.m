aa2ae6087c29e245f2bfa77862521998be495f47
Onofre Martorell <>
2016-11-03 20:30:46 +0100

    Fix bug for computing masks from sliding window

--- week4/Window_detection/create_mask_of_window.m
+++ week4/Window_detection/create_mask_of_window.m
@@ -1,13 +1,14 @@
 function [ new_mask ] = create_mask_of_window( BBox, Mask )
 dim = length(BBox);
 new_mask = (zeros(size(Mask)));
-for i = 1:dim
-    y = BBox(i).y;
-    x = BBox(i).x;
-    window_width = BBox(i).w;
-    window_height = BBox(i).h;
-    new_mask(y:window_width + y, x:window_height + x) = Mask(y:window_width + y, x:window_height + x);
+if BBox(1).x ~= 0
+    for i = 1:dim
+        y = BBox(i).y;
+        x = BBox(i).x;
+        window_width = BBox(i).w;
+        window_height = BBox(i).h;
+        new_mask(y:window_width + y, x:window_height + x) = Mask(y:window_width + y, x:window_height + x);
+    end
 end
-
 end
 
