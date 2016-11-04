5e1e5f56ea9fe7aef26b8d657695671b6b5692d3
Roque <roque.rodriguez.outeiral@gmail.com>
2016-11-04 23:35:19 +0100

    Revert "no message"
    This reverts commit 4f183d6d0247ccc76a87a9e815929a3f12423085.

--- week4/Template_Matching/template_model1.m
+++ week4/Template_Matching/template_model1.m
@@ -4,7 +4,7 @@ template=zeros(siz(1), siz(2));
 
 ry=siz(1)/2 -.5;
 rx=siz(2)/2 -.5;
-[x,y] = meshgrid(-rx-1:rx+1,-ry-1:ry+1);
+[x,y] = meshgrid(-rx:rx,-ry:ry);
 
 template = double((((x/rx).^2 + (y/ry).^2) <= 1));
 
