9b66f92167e789c624078bfd64862c992da97ec4
Lidia Talavera <lidia_tama@hotmail.com>
2016-11-04 14:14:29 +0100

    pruebas

--- week4/Template_Matching/Template.m
+++ week4/Template_Matching/Template.m
@@ -1,26 +1,27 @@
 %Task 1 w4
 function template = Template(signType,siz)
-
+siz=[32,42];
     switch signType
         case 1
+           
             template = template_model1(siz);
             template = edge(template,'canny');
-    %         figure();
-    %         imshow(circle);
+%             figure();
+%             imshow(template);
         case 2
             template = template_model2(siz);
             template = edge(template,'canny');
-    %         figure();
-    %         imshow(square);
+%             figure();
+%             imshow(template);
         case 3
             template = template_model3(siz);
             template = edge(template,'canny');
-    %         figure();
-    %         imshow(triangle);
+%             figure();
+%             imshow(template);
         case 4
             template = template_model4(siz);
             template = edge(template,'canny');
-    %         figure();
-    %         imshow(itriangle);
+%             figure();
+%             imshow(template);
     end
 end
--- week4/Template_Matching/template_model2.m
+++ week4/Template_Matching/template_model2.m
@@ -1,4 +1,5 @@
 function template = template_model2(siz)
 %Square Template
-template=ones(siz(1), siz(2));
+template = zeros(siz(1), siz(2));
+template(2:end-1,2:siz(2)-1)= 1;
 end
