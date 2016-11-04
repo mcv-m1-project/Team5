60c03f3333aa385e0c6498d02cf3480ea41478ca
Idoia Ruiz <idoiaruizl@gmail.com>
2016-11-04 14:03:33 +0100

    CCL: empty structs and discard by area

--- week4/Window_detection/Connected components/trainSignCharacteristicsCCL.m
+++ week4/Window_detection/Connected components/trainSignCharacteristicsCCL.m
@@ -6,6 +6,10 @@ function params = trainSignCharacteristicsCCL(SC)
 
 form_factor=[SC{:,3}];
 filling_ratio=[SC{:,4}];
+ann=[SC{:,6}];
+w = [ann.w];
+h = [ann.h];
+BB_area=w.*h;
 
 stdff=std(form_factor);
 maxff=max(form_factor)+stdff;
@@ -13,6 +17,8 @@ minff=min(form_factor)-stdff;
 stdfr=std(filling_ratio);
 maxfr=max(filling_ratio)+stdfr;
 minfr=min(filling_ratio)-stdfr;
+maxarea=max(BB_area);
+minarea=min(BB_area);
 
-params = struct('maxff', maxff, 'minff', minff, 'maxfr',maxfr, 'minfr', minfr);
+params = struct('maxff', maxff, 'minff', minff, 'maxfr',maxfr, 'minfr', minfr,'maxarea',maxarea, 'minarea', minarea);
 end
