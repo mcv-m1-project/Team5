2483bdc391a4de8f3c95527e9f2cf21291ed961d
Idoia Ruiz <idoiaruizl@gmail.com>
2016-11-06 20:04:05 +0100

    fix

--- week4/launch_w4_task1.m
+++ week4/launch_w4_task1.m
@@ -13,7 +13,7 @@ set_type = 'validate';
 
 %Names of the previous work to compute the correlation
 methods = { 'Global' 'CCLHSV'  'CCLHSVRGB'};
-meth = [       1             ];
+meth =    [    1        2           3     ];
 
 
 [params, files, templates] = compute_paremeters_w4( directory_results, directory_images, set_type, directory_templates );
