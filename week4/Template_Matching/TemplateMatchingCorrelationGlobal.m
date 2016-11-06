9fab5605abbb91441490662bded3b3ac9537558f
Idoia Ruiz <idoiaruizl@gmail.com>
2016-11-06 14:22:51 +0100

    task1

--- week4/Template_Matching/TemplateMatchingCorrelationGlobal.m
+++ week4/Template_Matching/TemplateMatchingCorrelationGlobal.m
@@ -1,12 +1,14 @@
 function [windowCandidates,mask]=TemplateMatchingCorrelationGlobal(im,templates,corr_threshold)
+%Find the peaks of correlation between the image and the templates, that
+%are higher than the threshold. This function  returns the window 
+%candidates and the generated mask.
     windowCandidates = [];
     mask=zeros(size(im));
     for i=1:length(templates)
         correlation = normxcorr2(templates{i},im);
         
         if max(max(correlation))>corr_threshold
-            %TODO: threshold (several wc), not only the max corr
-            [ypeak, xpeak] = find(correlation==max(correlation(:)));
+            [ypeak, xpeak] = find(correlation>corr_threshold);
 
             yoffSet = ypeak-size(templates{i},1);
             xoffSet = xpeak-size(templates{i},2);
@@ -14,8 +16,13 @@ function [windowCandidates,mask]=TemplateMatchingCorrelationGlobal(im,templates,
             yoffSet(yoffSet<0)=0;
             xoffSet(xoffSet+size(templates{i},2)>size(im,2))=size(im,2)-size(templates{i},2);
             yoffSet(yoffSet+size(templates{i},1)>size(im,1))=size(im,1)-size(templates{i},1);
-            new_WC = struct('x',xoffSet+1,'y',yoffSet+1,'w',size(templates{i},2),'h',size(templates{i},1));
-
+            
+            new_WC = struct('x',num2cell(xoffSet+1),'y',num2cell(yoffSet+1),'w',{size(templates{i},2)},'h',{size(templates{i},1)});
+            
+            %Remove repeated windowCandidates
+            [~,idxs,~] = unique(cell2mat(struct2cell(new_WC)'),'rows');
+            new_WC=new_WC(idxs);
+            
             windowCandidates=[windowCandidates;new_WC];
             
             %Create mask with template shape
