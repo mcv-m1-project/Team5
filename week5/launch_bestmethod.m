cae53b63d07cc6a108512ca9344d8062fed44fdc
Idoia Ruiz <idoiaruizl@gmail.com>
2016-11-13 19:29:52 +0100

    best method update

--- week5/launch_bestmethod.m
+++ week5/launch_bestmethod.m
@@ -13,28 +13,60 @@ set_type = 'validate';
 %%
 %Segmentation
 
-colorSp = 7;%Chosen segmentation method: histBP
+colorSp = [3 7];%Chosen segmentation method: HSV histBP
 [ params, files] = compute_paremeters_w1( directory_results, directory_images, set_type );
-
-params.colorSpace = colorSp;
-metr_method = SignDetection(params, files);
-
+for i = 1:length(colorSp)
+    params.colorSpace = colorSp(i);
+    metr_method = SignDetection(params, files);
+end
 %%
 
 %Morphological operators
-colorSp = 5;%Chosen segmentation method: histBP
+colorSp = [2 5];%Chosen segmentation method: HSV histBP
 [ params, files] = compute_paremeters_w2( directory_results, directory_images, set_type );
-
-params.colorSpace = colorSp;
-metr_method = SignDetection_t3w2(params, files);
-
+for i = 1:length(colorSp)
+    params.colorSpace = colorSp(i);
+    metr_method = SignDetection_t3w2(params, files);
+end
 
 %%
 
 %CCL
-colorSp =3;%Chosen segmentation method: histBP
+colorSp =[1 3];%Chosen segmentation method: HSV histBP
 [ params, files, SC_train ] = compute_paremeters_w3( directory_results, directory_images, set_type );
 
-params.colorSpace = colorSp;
-metr_method = SignDetectionCCL( params, files, SC_train );
-save(strcat(params.directory_write_results, '/metrix_methods_', params.type_set, '_CCL_hbp'), 'metr_method');
+for i = 1:length(colorSp)
+    params.colorSpace = colorSp(i);
+    metr_method = SignDetectionCCL( params, files, SC_train );
+end
+
+%%
+
+%OR
+
+hsvpath=strcat('../Results/week_03/',set_type,'_result/HSV_CCL');
+hsvfiles=dir(strcat(hsvpath,'/*.png'));
+
+hbppath=strcat('../Results/week_03/',set_type,'_result/histBP_CCL');
+hbpfiles=dir(strcat(hbppath,'/*.png'));
+
+writepath=strcat('../Results/week_05/',set_type,'_result/best_approach');
+if ~exist(writepath , 'dir')
+    mkdir(writepath);
+end
+
+for i=1:length(hsvfiles)
+    hsvmask = logical(double(imread(strcat(hsvpath, filesep, hsvfiles(i).name))));
+    hbpmask = logical(double(imread(strcat(hbppath, filesep, hbpfiles(i).name))));
+    newmask=double(hsvmask|hbpmask);
+    imwrite(newmask,strcat(writepath,filesep,hsvfiles(i).name));
+end
+
+%%
+
+%Metrics
+
+metrix_method = metrics_pixel(writepath,strcat(directory_images,filesep,'train'));
+
+save(strcat(writepath, '/metrix_methods_', params.type_set, '_bestapproach'), 'metrix_method');
+
