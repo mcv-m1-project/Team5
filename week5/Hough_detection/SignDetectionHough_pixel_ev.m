616c8b579d335abce1719dd6b6f3bc9d085a8e77
Onofre Martorell <>
2016-11-13 18:48:05 +0100

    fix 2.0

--- week5/Hough_detection/SignDetectionHough_pixel_ev.m
+++ week5/Hough_detection/SignDetectionHough_pixel_ev.m
@@ -12,8 +12,8 @@ switch params.colorSpace
         params.directory_read_window = strcat(params.directory_read_window, '/histBP_CCL/');
         params.directory_write_results = strcat(params.directory_write_results, '/HBP_CCL_Hough/');
     case 3
-        params.directory_read_mask = strcat(params.directory_read_window, '/UCM2/');
-        params.directory_read_window = strcat(params.directory_read_window, '/UCM2/');
+        params.directory_read_mask = strcat(params.directory_write_results, '/UCM2/');
+        params.directory_read_window = strcat(params.directory_write_results, '/UCM2/');
         params.directory_write_results = strcat(params.directory_write_results, '/UCM_Hough/');
 end
 
